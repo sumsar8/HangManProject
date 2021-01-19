a109d2928717021db15891acc0aae0241217436e
sumsar8 <60388964+sumsar8@users.noreply.github.com>
2021-01-19 11:10:41 +0100

    Update HangMan.java

--- HangMan.java
+++ HangMan.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
+
 public class HangMan {
     public static void main(String[] args) {
         String randomword = pickarandomword();
     }
-    private static String pickarandomword() {
 
+    private static String pickarandomword() {
+        File myObj = new File("words.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String Word = myReader.nextLine();
+            System.out.println(Word);
+        }
+        return null;
     }
 }
