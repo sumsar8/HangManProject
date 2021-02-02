1fecb2c035aabd93741a6d2e06433a26a728b6f0
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2021-02-02 11:06:30 +0100

    Jag har fixan en boolean loop som fortsätter fråga om bokstäver tills man har svarat rätt på alla bokstäver.

--- HangMan.java
+++ HangMan.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class HangMan {
     public static void main(String[] args) {
 
+        int correctguesses = 0;
         String answer;
         String[] randomwords = {"oga", "ploppsie", "ogabobpsie"};
 
@@ -17,12 +18,21 @@ public class HangMan {
             characters[i] = answer.charAt(i);
         }
 
+        boolean isrunning = true;
+        while (isrunning){
         String playerguess = JOptionPane.showInputDialog("What is your guess?");
         char playerguesschar = playerguess.charAt(0);
+
 
         for(int i = 0; i <= answer.length() - 1; i++){
         if(playerguesschar == answer.charAt(i)) {
             JOptionPane.showMessageDialog(null, answer.charAt(i));
+            correctguesses++;
+            System.out.println(correctguesses);
+        if(correctguesses == answer.length()){
+            isrunning = false;
+            }
+        }
             }
         }
     }
